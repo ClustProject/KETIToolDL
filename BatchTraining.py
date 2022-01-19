b4c6167ab5e5df6ab31df7198380f7de312ade74
jaewonMoon <jaewonmoon.keti@gmail.com>
2022-01-19 19:26:32 +0900

    train

--- BatchTraining.py
+++ BatchTraining.py
@@ -11,7 +11,7 @@ from KETIPreDataIngestion.KETI_setting import influx_setting_KETI as ins
 from KETIPreDataIngestion.data_influx import influx_Client
 
 ##########################################
-mode_list = ['MS_Training', 'DB_Training']
+
 model_method_list = ['brits']
 model_purpose_list=['imputation']
 ##########################################
@@ -24,12 +24,12 @@ first ='2020-06-18'
 last ='2020-06-19'
 ##########################################
 bind_params = {'end_time':last, 'start_time': first}
-mode = mode_list[0] ## mode select
 model_purpose = model_purpose_list[0]
 model_method = model_method_list[0]
-model_rootDir = os.path.join('/Users', 'bunnyjw','Git', 'DL', 'Models', model_purpose, model_method)
+model_rootDir = os.path.join('C:\\Users', 'bunny','Code_CLUST', 'DL', 'Models', model_purpose, model_method)
 ###########################################
-
 # 1. Training Influx DB Data
 from trainBySource import influxDBData
+mode_list = ['MS_Training', 'DB_Training']
+mode = mode_list[0] ## mode select
 influxDBData.train(model_method, mode, DBClient, db_name, ms_name, bind_params, model_rootDir)
