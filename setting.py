8ce0a080cb17929a888267973e36c6090a34eb9a
jwmmoonP <jaewonmoon.keti@gmail.com>
2022-03-17 17:47:32 +0900

    parameter change

--- setting.py
+++ setting.py
@@ -1,6 +1,8 @@
 # Modify this Path to make adaptive model path environment
-#myModelRootPath = ["/Users", "jw_macmini", "CLUSTGit", "DL", "Models"]
-myModelRootPath = ["c:","Users", "bunny", "Code_CLUST", "KETIToolDL","DL", "Models"]
+
+myModelRootPath = ["/Users", "jw_macmini", "CLUSTGit", "DL", "Models"]
+myModelRootPath = ["/Users", "bunnyjw", "Git", "DL", "Models"]
+#myModelRootPath = ["c:","Users", "bunny", "Code_CLUST", "KETIToolDL","DL", "Models"]
 myModelInfoList = {
             "brits":{
                 "modelRootPath": myModelRootPath, 
