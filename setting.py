2c2fc439cf50eb22ea5e26207594d2b0ce55928f
keymemroy <keymemory@naver.com>
2022-06-24 16:21:29 +0900

    Merge branch 'main' of https://github.com/ClustProject/KETIToolDL into main

--- setting.py
+++ setting.py
@@ -2,6 +2,11 @@
 
 #myModelRootPath = ["/Users", "jw_macmini", "CLUSTGit", "DL", "Models"]
 #myModelRootPath = ["/Users", "bunnyjw", "Git", "DL", "Models"]
+# myModelRootPath = ['/home','leezy', 'CLUST_KETI', 'KETIAppTestCode','JHTest', 'models']
+# myModelRootPath = ['/home','leezy','CLUST_KETI','KETIAppTestCode','JWTest', 'RNNStyleModelTest','models']
+# myModelRootPath = ['/home','leezy','CLUST_KETI','KETIAppDataServer','model_Inference','models']
+# myModelRootPath = ['/programdrive','ProjectServer','KETIAppDataServer','model_Inference','models']
+
 #myModelRootPath = ['/home','keti', 'CLUST_KETI', 'Clust', 'KETIAppTestCode','JHTest', 'Models']
 myModelRootPath = ['/home','keti', 'CLUST_KETI', 'Clust', 'KETIAppTestCode','JWTest', 'RNNStyleModelTest','Models']
 #myModelRootPath = ["c:","Users", "bunny", "Code_CLUST", "KETIToolDL","DL", "Models"]
