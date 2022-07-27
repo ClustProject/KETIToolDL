b3e47bcd077f6ca2b753172c2eafad6dd42620ed
jwmoonP <jaewonmoon.keti@gmail.com>
2022-07-27 19:20:24 +0900

    update Tool

--- setting.py
+++ setting.py
@@ -8,8 +8,8 @@
 # myModelRootPath = ['/programdrive','ProjectServer','KETIAppDataServer','model_Inference','models']
 
 #myModelRootPath = ['/home','keti', 'CLUST_KETI', 'Clust', 'KETIAppTestCode','JHTest', 'Models']
-#myModelRootPath = ['/home','keti', 'CLUST_KETI', 'Clust', 'KETIAppTestCode','JWTest','Models']
-myModelRootPath = ['/home', 'hwangjisoo', '바탕화면', 'Clust', 'KETIAppTestCode','JSTest', 'ClassificationTest','KUDataClassification-main','Models']
+myModelRootPath = ['/home','keti', 'CLUST_KETI', 'Clust', 'KETIAppTestCode','JWTest','Models']
+#myModelRootPath = ['/home', 'hwangjisoo', '바탕화면', 'Clust', 'KETIAppTestCode','JSTest', 'ClassificationTest','KUDataClassification-main','Models']
 #myModelRootPath = ["c:","Users", "bunny", "Code_CLUST", "KETIToolDL","DL", "Models"]
 myModelInfoList = {
             "brits":{
