f446e8f0de3140a00040f0ff4219d18e92c011af
jwmoonP <jaewonmoon.keti@gmail.com>
2022-08-02 21:13:46 +0900

    update

--- CLUSTTool/Regression/p4_testing.py
+++ CLUSTTool/Regression/p4_testing.py
@@ -3,15 +3,19 @@ sys.path.append("../")
 
 from KETIToolDL.CLUSTTool.common import p4_testing as p4
 from KETIToolDL.CLUSTTool.common import p2_dataSelection as p2
+import os
+def getTestResult(dataName_X, dataName_y, modelName, DataMeta, ModelMeta, dataFolderPath, currentFolderPath, device, windowNum=0, db_client=None):
 
-def getTestResult(dataName_X, dataName_y, modelName, DataMeta, ModelMeta, dataFolderPath, device, windowNum=0, db_client=None):
     dataSaveMode_X = DataMeta[dataName_X]["integrationInfo"]["DataSaveMode"]
     dataSaveMode_y = DataMeta[dataName_y]["integrationInfo"]["DataSaveMode"]
     dataX = p2.getSavedIntegratedData(dataSaveMode_X, dataName_X, dataFolderPath)
     datay = p2.getSavedIntegratedData(dataSaveMode_y, dataName_y, dataFolderPath)
-    X_scalerFilePath = ModelMeta[modelName]['files']['XScalerFile']["filePath"]
-    y_scalerFilePath = ModelMeta[modelName]['files']['yScalerFile']["filePath"]
-    modelFilePath = ModelMeta[modelName]['files']['modelFile']["filePath"]
+    X_scalerFilePath = os.path.join(currentFolderPath, ModelMeta[modelName]['files']['XScalerFile']["filePath"])
+    y_scalerFilePath = os.path.join(currentFolderPath, ModelMeta[modelName]['files']['yScalerFile']["filePath"])
+    modelFilePath_old = ModelMeta[modelName]['files']['modelFile']["filePath"]
+    modelFilePath =[]
+    for modelFilePath_one in modelFilePath_old:
+        modelFilePath.append(os.path.join(currentFolderPath, modelFilePath_one))
     featureList = ModelMeta[modelName]["featureList"]
     target = ModelMeta[modelName]["target"]
     scalerParam = ModelMeta[modelName]["scalerParam"]
