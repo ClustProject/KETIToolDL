052a03bf186dadb0c3ec7e90f7f71dfc43023118
hwangjisoo <ghkdwltn8@gmail.com>
2022-09-13 14:22:44 +0900

    update

--- CLUSTTool/Classification/p4_testing.py
+++ CLUSTTool/Classification/p4_testing.py
@@ -40,8 +40,8 @@ def getTestResult(dataName_X, dataName_y, modelName, DataMeta, ModelMeta, dataFo
     dataX = p2.getSavedIntegratedData(dataSaveMode_X, dataName_X, dataFolderPath)
     datay = p2.getSavedIntegratedData(dataSaveMode_y, dataName_y, dataFolderPath)
     
-    if modelName.split("_")[1] == "ML":
-        datay = datay.iloc[1:]
+    #if modelName.split("_")[1] == "ML":
+    #    datay = datay.iloc[1:]
     
     X_scalerFilePath = ModelMeta[modelName]['files']['XScalerFile']["filePath"]
     y_scalerFilePath = ModelMeta[modelName]['files']['yScalerFile']["filePath"]
